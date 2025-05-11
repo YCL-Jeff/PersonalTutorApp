837f7e7d5b4ef4676d9924e4b09fbff78ee8b20c
admin <admin@DESKTOP-TCFOB5E>
2025-05-11 22:22:12 +0100

    修改了Lesson字段

--- app/src/main/java/com/example/personaltutorapp/model/Lesson.kt
+++ app/src/main/java/com/example/personaltutorapp/model/Lesson.kt
@@ -3,8 +3,6 @@ package com.example.personaltutorapp.model
 data class Lesson(
     val lessonId: String? = null,
     val courseId: String = "",
-    val title: String = "",
     val content: String = "",
-    val order: Int = 0,
-    val isCompleted: Boolean = false
+
 )
